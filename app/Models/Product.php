7e10a80d3b9c999a5b91e3b0d2d1e6572c7166b6
ZeinaZayed4 <zeinazayed088@gmail.com>
2025-11-15 20:28:42 +0200

    Added front template

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Models\Scopes\StoreScope;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -42,5 +45,29 @@ class Product extends Model
             'id',
             'id'
         );
+    }
+
+    #[Scope]
+    protected function scopeActive(Builder $query): Builder
+    {
+        return $query->where('status', '=', 'active');
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image) {
+            return 'https://www.tiffincurry.ca/wp-content/uploads/2021/02/default-product.png';
+        }
+        if (Str::startsWith($this->image, ['http://', 'https://'])) {
+            return $this->image;
+        }
+        return asset('storage/' . $this->image);
+    }
+
+    public function getSalePercentAttribute(): float|int
+    {
+        if (!$this->compare_price)
+            return 0;
+        return round(100 - (100 * $this->price / $this->compare_price), 1);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,12 @@
 <?php
 
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\ProductController;
+
 require __DIR__.'/auth.php';
 require_once __DIR__.'/dashboard.php';
+
+Route::get('', [HomeController::class, 'index'])->name('home');
+
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
